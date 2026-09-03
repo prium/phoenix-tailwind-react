713580793fa2fadef2ffa46bb3fce5d10dbd6fd0
Khayrul Islam <khayrulislam2525@gmail.com>
2026-09-03 11:22:11 +0600

    refactor: spell the Lightbox props type like the component

--- src/components/base/Lightbox.tsx
+++ src/components/base/Lightbox.tsx
@@ -1,13 +1,13 @@
 import FsLightbox, { FsLightboxProps } from 'fslightbox-react';
 import { JSX } from 'react';
 
-interface LightBoxProps extends FsLightboxProps {
+interface LightboxProps extends FsLightboxProps {
   toggler: boolean;
   slide?: number;
   sources: Array<string | JSX.Element>;
 }
 
-const Lightbox = ({ toggler, slide = 1, sources, ...rest }: LightBoxProps) => {
+const Lightbox = ({ toggler, slide = 1, sources, ...rest }: LightboxProps) => {
   return (
     <FsLightbox toggler={toggler} sources={sources} slide={slide} {...rest} />
   );
